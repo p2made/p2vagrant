ab34d9784218163a09cd05ff202217a1d2438daa
p2made <pedrofp@me.com>
2021-08-23 21:05:04 +1000

    Update mysql.sh

--- provision/scripts/mysql.sh
+++ provision/scripts/mysql.sh
@@ -5,19 +5,23 @@ DBNAME=mydb
 DBUSER=myuser
 DBPASSWD=password
 
+# Install MySQL
 apt-get update
 
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
 
-apt-get -y install mysql-server-5.7
+apt-get -y install mysql-server
 
+# Create the database and grant privileges
 CMD="mysql -uroot -p$DBPASSWD -e"
 
 $CMD "CREATE DATABASE IF NOT EXISTS $DBNAME"
 $CMD "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'%' IDENTIFIED BY '$DBPASSWD';"
 $CMD "FLUSH PRIVILEGES;"
 
+# Allow remote access to the database
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 sudo service mysql restart
+
