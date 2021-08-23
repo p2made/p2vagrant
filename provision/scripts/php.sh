4f34e1b67b53cbc0e66725724ac997d97037bcb1
p2made <pedrofp@me.com>
2021-08-23 20:20:51 +1000

    Update php.sh

--- provision/scripts/php.sh
+++ provision/scripts/php.sh
@@ -2,6 +2,6 @@
 
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y php8.1 php8.1-mysql
+sudo apt-get install -y php8.0 php8.0-mysql
 
 sudo service apache2 restart
