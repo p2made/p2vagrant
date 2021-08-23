815d9a25c87f2253246c8bd48358d23a84995003
p2made <pedrofp@me.com>
2021-08-23 18:18:58 +1000

    Update provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -3,6 +3,7 @@
 apt-get update
 apt-get install -y lsb-release ca-certificates apt-transport-https software-properties-common
 add-apt-repository ppa:ondrej/php
+add-apt-repository ppa:ondrej/apache2
 
 apt-get update
 apt-get install -y apache2 mysql-server php8.0 php8.0-mysql
