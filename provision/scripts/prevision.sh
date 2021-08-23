55f86fdd7087b0b153378e97324b5b9c3391c796
p2made <pedrofp@me.com>
2021-08-23 21:36:24 +1000

    Update prevision.sh

--- provision/scripts/prevision.sh
+++ provision/scripts/prevision.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y lsb-release ca-certificates apt-transport-https software
 
 # language bug workaround
 sudo apt-get install -y language-pack-en-base
-export LC_ALL=en_AU.UTF-8
-export LANG=en_AU.UTF-8
+sudo export LC_ALL=en_AU.UTF-8
+sudo export LANG=en_AU.UTF-8
 sudo apt-get install -y software-properties-common
