2e3bf1890343ef25a3bdc02a526fd6ed7af7aa92
p2made <pedrofp@me.com>
2021-08-23 19:06:40 +1000

    Update repo.sh

--- provision/scripts/repo.sh
+++ provision/scripts/repo.sh
@@ -10,8 +10,3 @@ apt-get install -y language-pack-en-base
 export LC_ALL=en_AU.UTF-8
 export LANG=en_AU.UTF-8
 apt-get install -y software-properties-common
-
-apt-get update
-apt-get install -y apache2 mysql-server php8.0 php8.0-mysql
-#sudo service apache2 restart
-systemctl reload apache2
