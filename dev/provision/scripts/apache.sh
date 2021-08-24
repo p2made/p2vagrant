ad653109c6d18d55b783187f0ae596fcea3dc8e2
p2made <pedrofp@me.com>
2021-08-24 20:52:58 +1000

    Update apache.sh files

--- dev/provision/scripts/apache.sh
+++ dev/provision/scripts/apache.sh
@@ -5,20 +5,9 @@ sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 sudo apt-get update
 sudo apt-get install -y apache2
 
-yes | cp /var/www/provision/config/apache/vhosts/local.conf /etc/apache2/sites-available/
-#yes | cp /var/www/provision/config/ssl/* /etc/apache2/sites-available/
-
-
-
-
-
-#!/bin/bash
-
-apt-get update
-apt-get install -y apache2
-
 # Copy the vhost config file
-cp ./provision/config/apache/vhosts/mytestproject.local.com.conf /etc/apache2/sites-available/mytestproject.local.com.conf
+yes | cp ./provision/config/apache/vhosts/local.conf /etc/apache2/sites-available/
+#yes | cp /var/www/provision/config/ssl/* /etc/apache2/sites-available/
 
 # Disable the default vhost file
 a2dissite 000-default
@@ -29,21 +18,11 @@ a2ensite mytestproject.local.com.conf
 # Restart for the changes to take effect
 service apache2 restart
 
-
-
-
-#!/bin/bash
-
-apt-get update
-apt-get install -y apache2
-
-a2ensite local.conf
-a2dissite 000-default
-
-a2enmod rewrite
-sudo service apache2 restart
-
+# Remove Ubuntu /html folder
 rm -rf /var/www/html
 
+# Enable SSL
 sudo a2enmod ssl
+
+# Restart for the changes to take effect
 sudo service apache2 restart
--- provision/scripts/apache.sh
+++ provision/scripts/apache.sh
@@ -4,4 +4,3 @@ sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 
 sudo apt-get update
 sudo apt-get install -y apache2
-
