a80243f35b457f9b6dd3fca4204cf5e84d7b4396
p2made <pedrofp@me.com>
2021-08-24 22:28:18 +1000

    Update apache.sh

--- provision/components/apache.sh
+++ provision/components/apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 
-sudo apt-get update
-sudo apt-get install -y apache2
+apt-get update
+apt-get install -y apache2
