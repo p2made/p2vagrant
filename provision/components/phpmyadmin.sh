d9b40d273d55cc2d3ab459b472a0345cb6d40b43
p2made <pedrofp@me.com>
2021-08-25 14:10:59 +1000

    Update phpmyadmin.sh

--- provision/components/phpmyadmin.sh
+++ provision/components/phpmyadmin.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-sudo apt-get update
+apt-get update
+apt-get install -y unzip
 
 debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $2"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $2"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $2"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
 
-sudo apt-get install -y phpmyadmin php-mbstring php-gettext
+apt-get install -y phpmyadmin
 
-sudo phpenmod mbstring
-sudo systemctl restart apache2
+rm -rf /usr/share/phpmyadmin
+
+cd /tmp
+wget https://files.phpmyadmin.net/phpMyAdmin/$1/phpMyAdmin-$1-all-languages.zip
+unzip phpMyAdmin-$1-all-languages.zip
+rm phpMyAdmin-$1-all-languages.zip
+sudo mv phpMyAdmin-$1-all-languages $3/html/phpmyadmin
+
+sudo chown -R www-data:www-data $3/html/phpmyadmin
+sudo chmod -R 755 $3/html/phpmyadmin
+
+phpenmod mbstring
+systemctl restart apache2
