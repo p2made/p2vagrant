f02b075d3a2b8bd0a299aa702f3ed96c795b6f7b
p2made <pedrofp@me.com>
2021-09-02 18:54:44 +1000

    Update templates

--- provision/scripts/sites.sh
+++ provision/scripts/sites.sh
@@ -1,11 +1,76 @@
 #!/bin/bash
 
-yes | cp /var/www/provision/vhosts/* /etc/apache2/sites-available/
+openssl genrsa \
+  -out /var/www/provision/ssl/example.tld.key \
+  2048
+openssl req -new -x509 \
+  -key /var/www/provision/ssl/example.tld.key \
+  -out /var/www/provision/ssl/example.tld.cert \
+  -days 3650 \
+  -subj /CN=example.tld
+
+yes | cp /var/www/provision/vhosts/example.tld.conf /etc/apache2/sites-available/
+yes | cp /var/www/provision/ssl/example.tld.* /etc/apache2/sites-available/
 
 a2ensite example.tld.conf
+
+openssl genrsa \
+  -out /var/www/provision/ssl/example1.tld.key \
+  2048
+openssl req -new -x509 \
+  -key /var/www/provision/ssl/example1.tld.key \
+  -out /var/www/provision/ssl/example1.tld.cert \
+  -days 3650 \
+  -subj /CN=example1.tld
+
+yes | cp /var/www/provision/vhosts/example1.tld.conf /etc/apache2/sites-available/
+yes | cp /var/www/provision/ssl/example1.tld.* /etc/apache2/sites-available/
+
 a2ensite example1.tld.conf
+
+openssl genrsa \
+  -out /var/www/provision/ssl/example2.tld.key \
+  2048
+openssl req -new -x509 \
+  -key /var/www/provision/ssl/example2.tld.key \
+  -out /var/www/provision/ssl/example2.tld.cert \
+  -days 3650 \
+  -subj /CN=example2.tld
+
+yes | cp /var/www/provision/vhosts/example2.tld.conf /etc/apache2/sites-available/
+yes | cp /var/www/provision/ssl/example2.tld.* /etc/apache2/sites-available/
+
 a2ensite example2.tld.conf
+
+openssl genrsa \
+  -out /var/www/provision/ssl/example3.tld.key \
+  2048
+openssl req -new -x509 \
+  -key /var/www/provision/ssl/example3.tld.key \
+  -out /var/www/provision/ssl/example3.tld.cert \
+  -days 3650 \
+  -subj /CN=example3.tld
+
+yes | cp /var/www/provision/vhosts/example3.tld.conf /etc/apache2/sites-available/
+yes | cp /var/www/provision/ssl/example3.tld.* /etc/apache2/sites-available/
+
 a2ensite example3.tld.conf
+
+openssl genrsa \
+  -out /var/www/provision/ssl/example4.tld.key \
+  2048
+openssl req -new -x509 \
+  -key /var/www/provision/ssl/example4.tld.key \
+  -out /var/www/provision/ssl/example4.tld.cert \
+  -days 3650 \
+  -subj /CN=example4.tld
+
+yes | cp /var/www/provision/vhosts/example4.tld.conf /etc/apache2/sites-available/
+yes | cp /var/www/provision/ssl/example4.tld.* /etc/apache2/sites-available/
+
 a2ensite example4.tld.conf
 
+# Copy blocks for hosts ABOVE here
+
+# Restart Apache...
 sudo service apache2 restart
--- provision/scripts_/sites_template.sh
+++ provision/scripts_/sites_template.sh
@@ -12,14 +12,17 @@ openssl req -new -x509 \
   -key /var/www/provision/ssl/_hostname_.key \
   -out /var/www/provision/ssl/_hostname_.cert \
   -days 3650 \
-  -subj /CN=_hostname_
+  -subj "/CN=_hostname_" \
+  -addext "subjectAltName=DNS:_hostname_,DNS:*._hostname_,IP:10.0.0.1"
 
-yes | cp /var/www/provision/vhosts/_hostname_.conf /etc/apache2/sites-available/
-yes | cp /var/www/provision/ssl/_hostname_.* /etc/apache2/sites-available/
+cp /var/www/provision/vhosts/_hostname_.conf /etc/apache2/sites-available/
+cp /var/www/provision/ssl/_hostname_.* /etc/apache2/sites-available/
 
 a2ensite _hostname_.conf
 
 # / end for each host
+
+# Copy blocks for hosts ABOVE here
 
 # Restart Apache...
 sudo service apache2 restart
