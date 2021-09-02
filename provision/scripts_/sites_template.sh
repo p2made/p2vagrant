480e03fc3af29715035af664899634e91a714e72
p2made <pedrofp@me.com>
2021-09-02 18:42:34 +1000

    Update examples

--- provision/scripts_/sites_template.sh
+++ provision/scripts_/sites_template.sh
@@ -2,19 +2,24 @@
 
 # For each host...
 
-openssl req -x509 \
-  -newkey rsa:2048 \
-  -sha256 \
+# Copy this block to sites.sh
+# Replace _hostname_ with actual development host name
+
+openssl genrsa \
+  -out /var/www/provision/ssl/_hostname_.key \
+  2048
+openssl req -new -x509 \
+  -key /var/www/provision/ssl/_hostname_.key \
+  -out /var/www/provision/ssl/_hostname_.cert \
   -days 3650 \
-  -nodes \
-  -keyout /var/www/provision/ssl/_hostname_.key \
-  -out /var/www/provision/ssl/_hostname_.crt \
-  -subj "/CN=_hostname_"
+  -subj /CN=_hostname_
 
 yes | cp /var/www/provision/vhosts/_hostname_.conf /etc/apache2/sites-available/
 yes | cp /var/www/provision/ssl/_hostname_.* /etc/apache2/sites-available/
 
 a2ensite _hostname_.conf
+
+# / end for each host
 
 # Restart Apache...
 sudo service apache2 restart
