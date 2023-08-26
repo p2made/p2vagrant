cfeccd044100e42d32e5f00063a0e89058c889ca
p2made <pedrofp@me.com>
2023-08-26 16:07:06 +1000

    Update apache.sh

--- provision/scripts/apache.sh
+++ provision/scripts/apache.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
+
 apt-get update
 apt-get install -y apache2
 
 #yes | cp /var/www/provision/vhosts/local.conf /etc/apache2/sites-available/
 #yes | cp /var/www/provision/ssl/* /etc/apache2/sites-available/
-yes | cp ../provision/vhosts/local.conf /etc/apache2/sites-available/
-yes | cp ../provision/ssl/* /etc/apache2/sites-available/
 
 a2ensite local.conf
 a2dissite 000-default
