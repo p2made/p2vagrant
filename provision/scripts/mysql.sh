d0efedea2cc6c2eefa8127867bcfabd86d8a214f
p2made <pedrofp@me.com>
2023-08-27 17:07:23 +1000

    Update mysql.sh

--- provision/scripts/mysql.sh
+++ provision/scripts/mysql.sh
@@ -16,13 +16,13 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 apt-get -y install mysql-server
 
 # Create the database and grant privileges
-CMD="sudo mysql -uroot -p$rt_password -e"
+#CMD="sudo mysql -uroot -p$rt_password -e"
 
-$CMD "CREATE DATABASE IF NOT EXISTS $db_name"
-$CMD "CREATE DATABASE IF NOT EXISTS $db_name_test"
-$CMD "CREATE USER '$db_username@%' IDENTIFIED BY '$db_password';"
-$CMD "GRANT ALL PRIVILEGES ON *.* TO '$db_username@%';"
-$CMD "FLUSH PRIVILEGES;"
+#$CMD "CREATE DATABASE IF NOT EXISTS $db_name"
+#$CMD "CREATE DATABASE IF NOT EXISTS $db_name_test"
+#$CMD "CREATE USER '$db_username@%' IDENTIFIED BY '$db_password';"
+#$CMD "GRANT ALL PRIVILEGES ON *.* TO '$db_username@%';"
+#$CMD "FLUSH PRIVILEGES;"
 
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 grep -q "^sql_mode" /etc/mysql/mysql.conf.d/mysqld.cnf || echo "sql_mode = STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" >> /etc/mysql/mysql.conf.d/mysqld.cnf
