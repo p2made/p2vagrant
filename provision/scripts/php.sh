f948dd063d0c5545fe623631a5383b1cb62b7a76
p2made <pedrofp@me.com>
2023-09-01 00:35:54 +1000

    Update php.sh

--- provision/scripts/php.sh
+++ provision/scripts/php.sh
@@ -2,11 +2,21 @@
 
 # 04 Install PHP 8.2
 
+echo "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
+echo "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
+echo "#####                                                 #####"
+echo "#####             ¡¡¡ Installing PHP !!!              #####"
+echo "#####                                                 #####"
+echo "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
+echo "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
+
 apt-get install software-properties-common
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 
 apt-get update
 apt-get install -y php$1 php$1-mysql
+
+echo "¡¡¡ Configuring PHP..."
 
 sed -i 's/max_execution_time = .*/max_execution_time = 60/' /etc/php/$1/apache2/php.ini
 sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$1/apache2/php.ini
@@ -16,3 +26,5 @@ sed -i 's/display_errors = .*/display_errors = on/' /etc/php/$1/apache2/php.ini
 sed -i 's/display_startup_errors = .*/display_startup_errors = on/' /etc/php/$1/apache2/php.ini
 
 service apache2 restart
+
+echo "¡¡¡ PHP Installation Complete !!!"
