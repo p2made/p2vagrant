9e9a598030cac603c83bd12ea68ecb38b5b3dbbf
p2made <pedrofp@me.com>
2023-09-01 23:59:08 +1000

    Update mysql.sh

--- provision/scripts/mysql.sh
+++ provision/scripts/mysql.sh
@@ -28,11 +28,13 @@ apt-get -y install mysql-server
 echo "¡¡¡ Configuring MySQL..."
 
 # Create the databases, create user, & grant privileges
-sudo mysql -uroot -p$2 -e "CREATE USER '$3'@'%' IDENTIFIED BY '$4';"
-sudo mysql -uroot -p$2 -e "CREATE DATABASE IF NOT EXISTS $5"
-sudo mysql -uroot -p$2 -e "GRANT ALL PRIVILEGES ON $5.* TO '$3'@'%'"
-sudo mysql -uroot -p$2 -e "CREATE DATABASE IF NOT EXISTS $6"
-sudo mysql -uroot -p$2 -e "GRANT ALL PRIVILEGES ON $6.* TO '$3'@'%'"
+CMD="sudo mysql -uroot -p$2 -e"
+
+$CMD "CREATE USER '$3'@'%' IDENTIFIED BY '$4';"
+$CMD "CREATE DATABASE IF NOT EXISTS $5"
+$CMD "GRANT ALL PRIVILEGES ON $5.* TO '$3'@'%'"
+$CMD "CREATE DATABASE IF NOT EXISTS $6"
+$CMD "GRANT ALL PRIVILEGES ON $6.* TO '$3'@'%'"
 
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 grep -q "^sql_mode" /etc/mysql/mysql.conf.d/mysqld.cnf || echo "sql_mode = STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" >> /etc/mysql/mysql.conf.d/mysqld.cnf
