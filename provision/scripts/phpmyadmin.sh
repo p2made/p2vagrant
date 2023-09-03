7310a90316aae3dd62a00cc365b91a11a2a69278
p2made <pedrofp@me.com>
2023-09-04 00:04:05 +1000

    Update phpmyadmin.sh

--- provision/scripts/phpmyadmin.sh
+++ provision/scripts/phpmyadmin.sh
@@ -4,14 +4,13 @@
 
 apt-get update
 apt-get install -y unzip
+apt-get install -y phpmyadmin
 
 debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $2"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $2"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $2"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
-
-apt-get install -y phpmyadmin
 
 rm -rf /usr/share/phpmyadmin
 
