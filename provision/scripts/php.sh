1bf77f75ecefa7b04806c0adbc47dd8f96c21fca
p2made <pedrofp@me.com>
2023-09-03 19:00:34 +1000

    Update php.sh

--- provision/scripts/php.sh
+++ provision/scripts/php.sh
@@ -11,12 +11,11 @@ echo "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
 echo "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
 
 apt-get install software-properties-common
+
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 
 apt-get update
 apt-get install -y php$1 php$1-mysql
-
-echo "¡¡¡ Configuring PHP..."
 
 sed -i 's/max_execution_time = .*/max_execution_time = 60/' /etc/php/$1/apache2/php.ini
 sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$1/apache2/php.ini
@@ -26,5 +25,3 @@ sed -i 's/display_errors = .*/display_errors = on/' /etc/php/$1/apache2/php.ini
 sed -i 's/display_startup_errors = .*/display_startup_errors = on/' /etc/php/$1/apache2/php.ini
 
 service apache2 restart
-
-echo "¡¡¡ PHP Installation Complete !!!"
