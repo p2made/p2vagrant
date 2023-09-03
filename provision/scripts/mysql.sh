9ac424617690960c6786085c3ae6f32b9e313743
p2made <pedrofp@me.com>
2023-09-03 23:46:10 +1000

    Update mysql.sh

--- provision/scripts/mysql.sh
+++ provision/scripts/mysql.sh
@@ -31,12 +31,6 @@ $CMD "GRANT ALL PRIVILEGES ON $5.* TO '$3'@'%'"
 $CMD "CREATE DATABASE IF NOT EXISTS $6"
 $CMD "GRANT ALL PRIVILEGES ON $6.* TO '$3'@'%'"
 
-#mysql -uroot -p$2 -e "CREATE DATABASE IF NOT EXISTS $5;"
-#mysql -uroot -p$2 -e "CREATE DATABASE IF NOT EXISTS $6;"
-#mysql -uroot -p$2 -e "CREATE USER '$3@%' IDENTIFIED BY '$4';"
-#mysql -uroot -p$2 -e "GRANT ALL PRIVILEGES ON *.* TO '$3@%';"
-#mysql -uroot -p$2 -e "FLUSH PRIVILEGES;"
-
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 #grep -q "^sql_mode" /etc/mysql/mysql.conf.d/mysqld.cnf || echo "sql_mode = STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" >> /etc/mysql/mysql.conf.d/mysqld.cnf
 
