1cfab6ef39753065f4fe39a65ddd0c0cc8d9e118
p2made <pedrofp@me.com>
2023-09-05 23:47:37 +1000

    upgrade

--- provision/scripts/upgrade.sh
+++ provision/scripts/upgrade.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# 01 Upgrade
+
 apt-get update
 apt-get -y upgrade
 apt-get autoremove
 
-[ -f /var/run/reboot-required ] &amp;&amp; reboot -f
+[ -f /var/run/reboot-required ] && reboot -f
 
 cat /etc/os-release
