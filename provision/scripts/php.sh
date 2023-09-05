ebc042e5461ed14064f1bcf51c59f7cf94937ee9
p2made <pedrofp@me.com>
2023-09-06 01:19:41 +1000

    PHP

--- provision/scripts/php.sh
+++ provision/scripts/php.sh
@@ -4,9 +4,11 @@
 
 #PHP_VERSION         = $1 = 8.2
 
-apt-get install software-properties-common
-
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 
 apt-get update
-apt-get install -y php$1 php$1-mysql
+apt-get install -y php$1 php-common
+apt-get install -y php$1-{mysql,bcmath,fpm,xml,mysql,zip,intl,ldap,gd,cli,bz2,curl,mbstring,pgsql,opcache,soap,cgi,common,dom,imagick}
+
+php -v
+php -m
