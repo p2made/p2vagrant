372f74733ef84f277d53eab255c0f4a11ae8dde2
p2made <pedrofp@me.com>
2023-09-06 00:39:40 +1000

    Update php.sh

--- provision/scripts/php.sh
+++ provision/scripts/php.sh
@@ -10,12 +10,3 @@ LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 
 apt-get update
 apt-get install -y php$1 php$1-mysql
-
-sed -i 's/max_execution_time = .*/max_execution_time = 60/' /etc/php/$1/apache2/php.ini
-sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$1/apache2/php.ini
-sed -i 's/upload_max_filesize = .*/upload_max_filesize = 1G/' /etc/php/$1/apache2/php.ini
-sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php/$1/apache2/php.ini
-sed -i 's/display_errors = .*/display_errors = on/' /etc/php/$1/apache2/php.ini
-sed -i 's/display_startup_errors = .*/display_startup_errors = on/' /etc/php/$1/apache2/php.ini
-
-service apache2 restart
