ae3c17b2eeff05a7e041272adc9502a920953940
p2made <pedrofp@me.com>
2023-09-06 16:19:11 +1000

    Update db.php

--- shared/html/db.php
+++ shared/html/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "db_user", "db_password", "db");
+$conn = mysqli_connect("localhost", "fredspotty", "Pa$$w0rdTw0", "example_db");
 
 if (!$conn) {
 	die("Error: " . mysqli_connect_error());
