ddb8c2208944bce7ee8ae836f3fe585b91e3bb89
p2made <pedrofp@me.com>
2023-09-06 17:39:53 +1000

    Update utilities.sh

--- provision/scripts/utilities.sh
+++ provision/scripts/utilities.sh
@@ -4,4 +4,5 @@
 
 apt-get update
 
-apt-get install -y lsb-release gnupg2 ca-certificates apt-transport-https software-properties-common
+apt-get install -y lsb-release gnupg2 ca-certificates apt-transport-https unzip
+apt-get install software-properties-common -y
