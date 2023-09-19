b40581acef7b4806c8037937de30fdf789d6e06e
p2made <pedrofp@me.com>
2023-09-20 01:16:38 +1000

    Update install_apache.sh

--- provision/scripts/install_apache.sh
+++ provision/scripts/install_apache.sh
@@ -14,7 +14,10 @@ LC_ALL=C.UTF-8 apt-add-repository -yu ppa:ondrej/apache2
 
 apt-get update
 
-apt-get -qy install apache2 apache2-bin apache2-data apache2-utils
+apt-get -qy install apache2
+apt-get -qy install apache2-bin
+apt-get -qy install apache2-data
+apt-get -qy install apache2-utils
 
 yes | cp /var/www/provision/vhosts/local.conf /etc/apache2/sites-available/
 yes | cp /var/www/provision/html/index.html /var/www/html/index.htm
