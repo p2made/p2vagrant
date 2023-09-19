bd211271e67eebb4b90bc70faf646f3d9cefa15d
p2made <pedrofp@me.com>
2023-09-20 01:05:03 +1000

    Update install_php.sh

--- provision/scripts/install_php.sh
+++ provision/scripts/install_php.sh
@@ -14,9 +14,30 @@ echo -e "##### ##### ##### ##### #####\n\n"
 
 LC_ALL=C.UTF-8 apt-add-repository -yu ppa:ondrej/php
 
-apt-get -qy install php$1 php-common
-apt-get -qy install php$1-{bcmath,bz2,cgi,cli,common,curl,dom,exif,fileinfo,fpm,gd,iconv,imagick,imap,intl,ldap,mbstring,mcrypt,mysql,mysqli,opcache,pgsql,pspell,readline,simplexml,soap,xml,xmlreader,xmlrpc,zip}
-apt-get -qy install php-pear libapache2-mod-php$1
+apt -qy install php$1
+
+apt -qy install php$1-bcmath
+apt -qy install php$1-bz2
+apt -qy install php$1-cgi
+apt -qy install php$1-curl
+apt -qy install php$1-dom
+apt -qy install php$1-fpm
+apt -qy install php$1-gd
+apt -qy install php$1-imagick
+apt -qy install php$1-imap
+apt -qy install php$1-intl
+apt -qy install php$1-ldap
+apt -qy install php$1-mbstring
+apt -qy install php$1-mcrypt
+apt -qy install php$1-mysql
+apt -qy install php$1-pgsql
+apt -qy install php$1-pspell
+apt -qy install php$1-soap
+apt -qy install php$1-xmlrpc
+apt -qy install php$1-zip
+
+apt -qy install php-pear
+apt -qy install libapache2-mod-php$1
 
 sed -i 's/max_execution_time = .*/max_execution_time = 60/' /etc/php/$1/apache2/php.ini
 sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$1/apache2/php.ini
