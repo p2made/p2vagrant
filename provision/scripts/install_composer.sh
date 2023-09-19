0f001002fb7182d040a08b7ad453186bc78a4159
p2made <pedrofp@me.com>
2023-09-20 01:35:42 +1000

    Create install_composer.sh

---
+++ provision/scripts/install_composer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# 03a Install Composer
+
+echo -e "##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
+echo -e "##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
+echo -e "#####"
+echo -e "#####       Installing Composer"
+echo -e "#####"
+echo -e "##### ##### ##### ##### ##### #####"
+echo -e "##### ##### ##### ##### #####\n\n"
+
+EXPECTED_CHECKSUM   = "$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+ACTUAL_CHECKSUM     = "$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+	>&2 echo 'ERROR: Invalid installer checksum'
+	rm composer-setup.php
+	exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
