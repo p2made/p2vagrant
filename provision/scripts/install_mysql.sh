2fa4543b82af3500d6498970200b9cc62c291144
p2made <pedrofp@me.com>
2023-09-24 00:22:54 +1000

    Update install_mysql.sh

--- provision/scripts/install_mysql.sh
+++ provision/scripts/install_mysql.sh
@@ -5,7 +5,7 @@
 echo "##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
 echo "##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
 echo "#####                                                       #####"
-echo "#####       Installing MySQL                                #####"
+echo "#####       Installing MySQL $1                            #####"
 echo "#####                                                       #####"
 echo "##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
 echo "##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####"
@@ -13,17 +13,23 @@ echo ""
 
 export DEBIAN_FRONTEND=noninteractive
 
+# MYSQL_VERSION       = "8.1"                 | $1
+# DB_USERNAME         = "fredspotty"          | $2
+# DB_PASSWORD         = "Passw0rd"            | $3
+# DB_NAME             = "example_db"          | $4
+# DB_NAME_TEST        = "example_db_test"     | $5
+
 apt-get update
 
 apt-get -qy install mysql-server
 
 # Create the database and grant privileges
-echo "CREATE USER '$2'@'%' IDENTIFIED BY '$3'" | mysql
-echo "CREATE DATABASE IF NOT EXISTS $4" | mysql
-echo "CREATE DATABASE IF NOT EXISTS $5" | mysql
-echo "GRANT ALL PRIVILEGES ON $4.* TO '$2'@'%';" | mysql
-echo "GRANT ALL PRIVILEGES ON $5.* TO '$2'@'%';" | mysql
-echo "flush privileges" | mysql
+echo "CREATE USER '$2'@'%' IDENTIFIED BY '$3'"      | mysql
+echo "CREATE DATABASE IF NOT EXISTS $4"             | mysql
+echo "CREATE DATABASE IF NOT EXISTS $5"             | mysql
+echo "GRANT ALL PRIVILEGES ON $4.* TO '$2'@'%';"    | mysql
+echo "GRANT ALL PRIVILEGES ON $5.* TO '$2'@'%';"    | mysql
+echo "flush privileges"                             | mysql
 
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
