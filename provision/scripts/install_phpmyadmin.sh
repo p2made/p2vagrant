fb9773a802544e09279812cd4d86bcaa75467db7
p2made <pedrofp@me.com>
2023-09-25 21:42:43 +1000

    Update install_phpmyadmin.sh
    Lotsa echos for debugulation.

--- provision/scripts/install_phpmyadmin.sh
+++ provision/scripts/install_phpmyadmin.sh
@@ -17,21 +17,33 @@ export DEBIAN_FRONTEND=noninteractive
 # DB_PASSWORD         = "PM4Passw0rd"         | $2
 # REMOTE_FOLDER       = "/var/www"            | $3
 
+echo "LC_ALL=C.UTF-8 apt-add-repository -yu ppa:phpmyadmin/ppa"
 LC_ALL=C.UTF-8 apt-add-repository -yu ppa:phpmyadmin/ppa
 
+echo "debconf-set-selections <<< \"phpmyadmin phpmyadmin/dbconfig-install boolean true\""
 debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+echo "debconf-set-selections <<< \"phpmyadmin phpmyadmin/app-password-confirm password $2\""
 debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $2"
+echo "debconf-set-selections <<< \"phpmyadmin phpmyadmin/mysql/admin-pass password $2\""
 debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $2"
+echo "debconf-set-selections <<< \"phpmyadmin phpmyadmin/mysql/app-pass password $2\""
 debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $2"
+echo "debconf-set-selections <<< \"phpmyadmin phpmyadmin/reconfigure-webserver multiselect none\""
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
 
+echo "apt-get -qy install phpmyadmin"
 apt-get -qy install phpmyadmin
 
+echo "rm -rf /usr/share/phpmyadmin"
 rm -rf /usr/share/phpmyadmin
 
+echo "cp -R $3/provision/html/phpmyadmin $3/html/phpmyadmin"
 cp -R $3/provision/html/phpmyadmin $3/html/phpmyadmin
 
+echo "sudo chmod -R 755 $3/html/phpmyadmin"
 sudo chmod -R 755 $3/html/phpmyadmin
 
+echo "sudo phpenmod mbstring"
 sudo phpenmod mbstring
+echo "service apache2 restart"
 service apache2 restart
