a38acc072aef93829e219a5a96d7154419200bb1
p2made <pedrofp@me.com>
2023-09-26 03:03:25 +1000

    pma

--- provision/scripts/install_phpmyadmin.sh
+++ provision/scripts/install_phpmyadmin.sh
@@ -19,9 +19,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 LC_ALL=C.UTF-8 apt-add-repository -yu ppa:phpmyadmin/ppa
 
-#temp
-apt-get -qy install debconf-utils
-
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/app-password-confirm password $2" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password $2" | debconf-set-selections
@@ -37,4 +34,5 @@ cp -R $3/provision/html/phpmyadmin $3/html/phpmyadmin
 sudo chmod -R 755 $3/html/phpmyadmin
 
 sudo phpenmod mbstring
-service apache2 restart
+#service apache2 restart
+systemctl restart apache2
