deda713eca3cb3afd3529fc956ad3bfb90270884
p2made <pedrofp@me.com>
2024-01-19 01:08:16 +1000

    Update install_composer.sh

--- provision/scripts/install_composer.sh
+++ provision/scripts/install_composer.sh
@@ -16,8 +16,5 @@ export DEBIAN_FRONTEND=noninteractive
 # Update package lists
 apt-get update
 
-# Install dependencies
-sudo apt-get install -y curl php-cli php-mbstring unzip
-
 # Download and install Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
