7855450139ee24270daeb1413413be0ff4a92f99
p2made <pedrofp@me.com>
2024-02-04 17:54:47 +1000

    Update configure_sites.fish

--- provision/scripts/configure_sites.fish
+++ provision/scripts/configure_sites.fish
@@ -30,7 +30,7 @@ set -x DEBIAN_FRONTEND noninteractive
 # that we will quickly use & then erase.
 # Usage: setup_site_variables $one_site
 function setup_site_variables
-	# Use the passed string $one_site to set...
+	# Use the passed string $one_site to set a temporary global...
 	# $site_info_temp[1-6], where...
 	# $site_info_temp[1] is the domain
 	# $site_info_temp[2] is the reverse domain
@@ -79,6 +79,9 @@ function write_vhosts_file
 		-e "s|{{SSL_FILENAME}}|$argv[6]|g" \
 		-e "s|{{TODAYS_DATE}}|$TODAYS_DATE|g" \
 		$PROVISION_TEMPLATES/$argv[4] > $PROVISION_VHOSTS/$argv[5]
+
+	# Output progress message
+	echo "Vhosts file for $argv[1] created at $PROVISION_VHOSTS/$argv[5]"
 end
 
 # Function to generate SSL files
@@ -94,6 +97,9 @@ function generate_ssl_files
 		handle_error "Failed to generate SSL key for $argv[1]"
 	end
 
+	# Output progress message
+	echo "SSL key for $argv[1] generated at $PROVISION_SSL/$argv[6].key"
+
 	# Generate self-signed SSL certificate
 	if not openssl req -new -x509 \
 		-key $PROVISION_SSL/$argv[6].key \
@@ -102,6 +108,8 @@ function generate_ssl_files
 		-subj /CN=$argv[1]
 		handle_error "Failed to generate SSL certificate for $argv[1]"
 	end
+	# Output progress message
+	echo "SSL certificate for $argv[1] generated at $PROVISION_SSL/$argv[6].cert"
 end
 
 # Function to configure a website with everything done so far
@@ -122,6 +130,9 @@ function configure_website
 
 	# Enable site
 	sudo a2ensite $argv[5]
+
+	# Output progress message
+	echo "Website configured for $argv[1]"
 end
 
 for one_site in (cat $site_data_file | grep -v '^#')
@@ -133,12 +144,14 @@ for one_site in (cat $site_data_file | grep -v '^#')
 	set site_info $site_info_temp
 	set -e site_info_temp
 
-	# site_info[1] - domain
-	# site_info[2] - reverse domain
-	# site_info[3] - underscore domain
-	# site_info[4] - template filename
-	# site_info[5] - vhosts filename
-	# site_info[6] - SSL base filename
+	# Output progress message
+	echo "Variables for $site_info[1]..."
+	echo "\$site_info[1] - domain            = $site_info[1]"
+	echo "\$site_info[2] - reverse domain    = $site_info[2]"
+	echo "\$site_info[3] - underscore domain = $site_info[3]"
+	echo "\$site_info[4] - template filename = $site_info[4]"
+	echo "\$site_info[5] - vhosts filename   = $site_info[5]"
+	echo "\$site_info[6] - SSL base filename = $site_info[6]"
 
 	# Now all the data for a site is in `site_info`
 	# Within functions, replace 'site_info' with 'argv'
@@ -146,7 +159,7 @@ for one_site in (cat $site_data_file | grep -v '^#')
 	# Now go configure some web sites
 	write_vhosts_file  $site_info
 	generate_ssl_files $site_info
-
+	configure_website  $site_info
 end
 
 # Restart Apache after all configurations
