490fb7d86e1a1fbf0279c18b7f6e039ef098e1cb
p2made <pedrofp@me.com>
2024-02-13 03:17:12 +1000

    Create vagrantfiles_data.sh

---
+++ provision/data/vagrantfiles_data.sh
@@ -0,0 +1,12 @@
+# Sites data
+# one step per pipe | separated line
+# step_num "title"
+1|Create Bare VM
+2|Upgrade VM
+3|Install Utilities
+4|Upgrade VM (revisited)
+5|Install Swift
+6|Install Apache (with SSL)
+7|Install PHP (with Composer)
+8|Install MySQL
+10|Configure Sites
