223b61d778fd512c2a9c07f0284fd79949e213b3
p2made <pedrofp@me.com>
2024-02-14 16:51:31 +1000

    bashing

--- provision/scripts/install_apache.sh
+++ provision/scripts/install_apache.sh
@@ -11,6 +11,7 @@ job_complete="Apache Installed (with SSL 🔐 & Markdown 📄 🎊)"
 # Source common functions
 source /var/www/provision/scripts/_banners.sh
 source /var/www/provision/scripts/_common.sh
+source /var/www/provision/scripts/_sites.sh
 
 # Arguments...
 VM_HOSTNAME="$1"
@@ -104,48 +105,6 @@ function configure_default_website() {
 	a2dissite 000-default
 }
 
-# Function to configure the default website
-# Usage: configure_default_website
-function configure_default_website
-	# We have the data all as we want it, but in a global variable
-	# Put it in local variables, & erase the global variable
-	set domain            $VM_HOSTNAME
-	set underscore_domain "html"
-	set template_filename "0.conf"
-	set vhosts_filename   "local.conf"
-	set ssl_base_filename "$domain"_"$TODAYS_DATE"
-
-	# Now go configure some web sites
-	generate_vhosts_file \
-		$domain \
-		$underscore_domain \
-		$template_filename \
-		$vhosts_filename \
-		$ssl_base_filename
-	generate_ssl_files \
-		$domain \
-		$ssl_base_filename
-	configure_website \
-		$domain \
-		$underscore_domain \
-		$vhosts_filename \
-		$ssl_base_filename
-
-	# Check if index.html exists in the html folder
-	if not test -e $SHARED_HTML/index.html
-		cp $PROVISION_HTML/index.html $SHARED_HTML/
-	end
-
-	# Set permissions on web server files
-	chmod -R 755 $SHARED_HTML/*
-
-	# Enable the new site
-	a2ensite local.conf
-
-	# Disable the default site
-	a2dissite 000-default
-end
-
 # -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- #
 
 function provision() {
@@ -165,40 +124,3 @@ function provision() {
 }
 
 provision
-
-# -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- #
-
-#!/bin/fish
-
-# 05 Install Apache (with SSL & Markdown)
-
-# Function to install Apache
-# Usage: install_apache
-function install_apache
-	# Add repository for ondrej/apache2
-	LC_ALL=C.UTF-8 apt-add-repository -yu ppa:ondrej/apache2
-
-	# Update package lists & install packages
-	update_and_install_packages $package_list
-
-	# Enable required Apache modules
-	a2enmod rewrite
-	a2enmod ext_filter
-	a2enmod ssl
-
-	announce_success "Apache packages installed successfully!"
-
-	# Install Markdown rendering packages
-	update_and_install_packages $markdown_packages
-
-	# Add configuration for handling Markdown files
-	echo "AddType text/html .md" >> /etc/apache2/apache2.conf
-
-	# Add handler for .md files
-	echo "AddHandler cgi-script .md" >> /etc/apache2/conf-available/markdown.conf
-	a2enconf markdown
-
-	announce_success "Markdown rendering packages installed successfully!"
-end
-
-# -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- --
