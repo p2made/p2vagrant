c55e6b9d1f77e91bb21c0a782ee343b8c2b8dec8
p2made <pedrofp@me.com>
2024-02-17 05:13:50 +1000

    vm script

--- provision/data/vagrantfiles_data.sh
+++ provision/data/vagrantfiles_data.sh
@@ -27,3 +27,26 @@ DB_USERNAME="fredspotty"
 DB_PASSWORD="Passw0rd"
 DB_NAME="example_db"
 DB_NAME_TEST="example_db_test"
+
+# -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- #
+# -- -- /%/ -- -- /%/           ¡¡¡ DO NOT EDIT BELOW !!! /%/ -- -- /%/ -- -- /%/ -- -- #
+# -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- #
+
+# Sparse array of Vagrantfiles, indexed by setup step...
+vagrantfiles[1]="Create Bare VM"
+vagrantfiles[2]="Upgrade VM"
+vagrantfiles[3]="Install Utilities"
+vagrantfiles[4]="Install Swift (optional)"
+vagrantfiles[5]="Install Apache (with SSL & Markdown)"
+vagrantfiles[6]="Install PHP (with Composer)"
+vagrantfiles[7]="Install MySQL"
+vagrantfiles[9]="Configure Sites"
+
+# Sparse array of provisioning script calls, indexed by setup step...
+provisioning_indexes=(3 4 5 6 7 9)
+provisioning_items[3]='	config.vm.provision :shell, path: "provision/scripts/install_utilities.sh", args: [VM_HOSTNAME, TIMEZONE]'
+provisioning_items[4]='	config.vm.provision :shell, path: "provision/scripts/install_swift.sh", args: [SWIFT_VERSION]'
+provisioning_items[5]='	config.vm.provision :shell, path: "provision/scripts/install_apache.sh"'
+provisioning_items[6]='	config.vm.provision :shell, path: "provision/scripts/install_php.sh", args: [PHP_VERSION]'
+provisioning_items[7]='	config.vm.provision :shell, path: "provision/scripts/install_mysql.sh", args: [MYSQL_VERSION, PHP_VERSION, ROOT_PASSWORD, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_NAME_TEST]'
+provisioning_items[9]='	config.vm.provision :shell, path: "provision/scripts/configure_sites.sh"'
