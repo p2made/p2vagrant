c5bf86bf1840518e12dfb9c4715eec8061b497c0
p2made <pedrofp@me.com>
2024-02-18 20:22:12 +1000

    Update vm_data.sh

--- provision/data/vm_data.sh
+++ provision/data/vm_data.sh
@@ -1,40 +1,8 @@
 #!/bin/zsh
 
-# vagrantfiles_data.sh
-# Updated: 2024-02-15
+# vm_data.sh
 
 # Data for Vagrantfil generation.
-
-VM_TLDS=(
-	"test"                              # One line for every dev TLD used
-)
-
-# Machine Variables
-VM_HOSTNAME="p2vagrant"
-VM_IP="192.168.22.42"                   # 22 = titanium, 42 = Douglas Adams's number
-TIMEZONE="Australia/Brisbane"           # "Europe/London"
-MEMORY=4096
-CPUS=1
-
-# Synced Folders
-HOST_FOLDER="."
-VM_FOLDER="/var/www"
-
-# Software Versions
-PHP_VERSION="8.3"
-MYSQL_VERSION="8.3"
-SWIFT_VERSION="5.9.2"                   # For installing Swift (optional)
-
-# Database Variables
-ROOT_PASSWORD="RootPassw0rd"
-DB_USERNAME="fredspotty"
-DB_PASSWORD="Passw0rd"
-DB_NAME="example_db"
-DB_NAME_TEST="example_db_test"
-
-# -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- #
-# -- -- /%/ -- -- /%/           ¡¡¡ DO NOT EDIT BELOW !!! /%/ -- -- /%/ -- -- /%/ -- -- #
-# -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- /%/ -- -- #
 
 # Sparse array of Vagrantfiles, indexed by setup step...
 VAGRANTFILES_INDEXES=(1 2 3 4 5 6 7 9)
